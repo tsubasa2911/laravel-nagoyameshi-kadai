888d34763e04d8bab4fcf2b762e0223bc659583d
tubasa2911 <randoctor11@gmail.com>
2025-01-22 21:52:47 +0900

    会員側の認証を実装

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -25,10 +25,15 @@ class UserFactory extends Factory
     {
         return [
             'name' => fake()->name(),
+            'kana' => fake()->KanaName(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
+            'postal_code' => fake()->postcode(),
+            'address' => fake()->address(),
+            'phone_number' => fake()->PhoneNumber(),
+            'occupation' => fake()->word(), // occupationフィールドを追加
         ];
     }
 
