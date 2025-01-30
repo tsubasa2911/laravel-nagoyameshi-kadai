268e9448a12e4363e6123a2dd5735fe9dff8b46f
tubasa2911 <randoctor11@gmail.com>
2025-01-30 16:47:01 +0900

    管理者側の会員機能を作成

---
+++ app/Http/Controllers/Admin/UserController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use App\Http\Controllers\Controller;
+
+class UserController extends Controller
+{
+    public function index(Request $reqest) 
+    {
+        $keyword = $reqest->input('keyword');
+
+        if ($keyword !== null) {
+            $users = User::where('name', 'like', "%{$keyword}%")
+            ->orwhere('kana', 'like', "%{$keyword}%")->paginate(15);
+        } else {
+            // キーワードが空の場合は、全レコードを取得
+            $users = User::paginate(15); 
+        }    
+
+        // 全レコード取得
+        $total = $users->total();
+
+        return view('users.index', compact('users', 'keyword', 'total'));
+    }
+
+    public function show(User $user)
+    {
+        return view('users.show', compact('user'));
+    }
+
+}
---
+++ tests/Feature/Admin/UserTest.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Tests\Feature\Admin;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Hash;
+use Tests\TestCase;
+use App\Models\User;
+use App\Models\Admin;
+
+
+
+
+class UserTest extends TestCase
+{
+    use RefreshDatabase;
+
+    // 会員一覧ページ
+    public function test_guest_cannot_access_admin_user_index_page()
+    {
+        $response = $this->get(route('admin.users.index'));  //未ログインで会員一覧ページにアクセス
+        $response->assertRedirect(route('admin.login'));  //ログインページにリダイレクト
+    }
+
+    public function test_user_cannot_ccess_admin_user_index_page()
+    {
+        $user = User::factory()->create(); //一般ユーザーを作成
+        $response = $this->actingAs($user)->get(route('admin.users.index')); // 一般ユーザーで会員一覧ページにアクセス
+        $response->assertRedirect(route('admin.login'));
+    }
+
+    public function test_admin_user_can_access_admin_user_index_page()
+    {
+    
+        $response = $this->actingAs($admin)->get(route('admin.users.index')); // 管理者ユーザーで会員一覧ページにアクセス
+        $response->assertStatus(200); // 200 OK
+    }
+
+    // 会員詳細ページ
+    public function test_guest_cannot_access_admin_user_show_page()
+    {
+        $user = User::factory()->create(); // ユーザーを作成
+        $response = $this->get( route('admin.users.show', $user)); // 未ログインで会員詳細ページにアクセス
+        $response->assertRedirect(route('admin.login')); // ログインページにリダイレクト
+    }
+
+    public function test_normal_user_cannot_access_admin_user_show_page()
+    {
+        $user = User::factory()->create(); // 一般ユーザーを作成
+        $response = $this->actingAs($user)->get(route('admin.users.show', $user)); // 一般ユーザーで会員詳細ページにアクセス
+        $response->assertRedirect(route('admin.login'));
+    }
+
+    public function test_admin_user_can_access_admin_user_show_page()
+    {
+        $user = User::factory()->create(); // ユーザーを作成
+    
+        $response = $this->actingAs($admin)->get(route('admin.users.show', $user)); // 管理者ユーザーで会員詳細ページにアクセス
+        $response->assertStatus(200); // 200 OK
+    }
+}
